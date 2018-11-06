575edd43a57365846151cdf0a31236805384c2c7
pablopodgaiz <pablopodgaiz@gmail.com>
2018-11-06 17:28:24 -0300

    faltaba agregar el metodo play

--- src/opciones/Opcion_Reproducir_Melodia.java
+++ src/opciones/Opcion_Reproducir_Melodia.java
@@ -6,6 +6,8 @@ import org.jfugue.pattern.Pattern;
 
 import entidades.AnoteManagger;
 import entidades.Melodia;
+import entidades.MyPattern;
+import entidades.PlayerSingleton;
 import funciones_helper.Funcion_Helper;
 import repository.IMelodia_Repository;
 import repository.INota_Repository;
@@ -22,11 +24,12 @@ public class Opcion_Reproducir_Melodia extends Opcion {
 	@Override
 	public void ejecutar(AnoteManagger manager, Scanner input, Pattern pattern) {
 		Melodia melodia;
-		//Listar las notas
-		System.out.println("¿Cuál de las siguientes melodías desea modificar?");
+		// Listar las notas
+		System.out.println("¿Cuál de las siguientes melodías desea reproducir?");
 		manager.listarCanciones();
 		String nombreMelodia = Funcion_Helper.pedirString(MENSAJE_NOMBRE_MELODIA, input);
-		
+		manager.play(nombreMelodia, (MyPattern) pattern, PlayerSingleton.getInstance());
+
 	}
 
 }
